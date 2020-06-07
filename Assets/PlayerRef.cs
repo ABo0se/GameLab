205162f3541248113221ee538a34aa32935b2143
ABoOse <thanawat.65938@mail.kmutt.ac.th>
2020-06-08 03:07:08 +0700

    teststable1

--- Assets/PlayerRef.cs
+++ Assets/PlayerRef.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class PlayerRef : MonoBehaviour
 {
     public static int money;
     private void Start()
     {
-        money = 5250;
+        money = 500;   
     }
 
 }
